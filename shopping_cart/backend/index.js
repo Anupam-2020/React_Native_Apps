a55f746882688687852e4c6b73d428f6b729054f
Anupam-2020 <anupamanand.696@gmail.com>
2023-09-14 12:44:29 +0530

    added native folder

--- shopping_cart/backend/index.js
+++ shopping_cart/backend/index.js
@@ -3,5 +3,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+const PORT = 8000;
 
 app.use(cors())
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+})
